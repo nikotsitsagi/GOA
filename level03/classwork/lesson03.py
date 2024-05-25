228c29b0184c3d569469da117bf53c85192b82b0
nikotsitsagi <nikodalevani@gmail.com>
2024-05-25 15:24:28 +0400

    GOA

---
+++ level03/classwork/lesson03.py
@@ -0,0 +1,29 @@
+num1 = 2
+num2 = 3
+num3 = 8.2
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num3 % num1)
+
+name1= "niko"
+surname= "tsitsagi"
+print(name1 + surname)
+
+name = "niko"
+age = 15
+height = 1.72
+is_futureprogrammer = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_futureprogrammer))
+
+
+
+username = "magician"
+points = 50
+lives = 3
+print(username)
+print(points)
