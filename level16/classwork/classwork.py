39f541d2217ec6a50eccd45f191e7c3fb8aa97f9
nikotsitsagi <nikodalevani@gmail.com>
2024-08-29 21:37:57 +0400

    GOA

--- level16/classwork/classwork.py
+++ level16/classwork/classwork.py
@@ -25,5 +25,33 @@ def person_age(age):
         return "you are a child"
     age = input("enter your age here: ")
     print()
+#6)
+def check_passing_grade(score):
+    if score >= 80:
+        return "passed"
+    else:
+        return "not passed"
+
+user_score = 85
+print(check_passing_grade(user_score))
+#7)
+from turtle import *
+def draw_square(length):
+    for i in range(4):
+        forward(length)
+        right(90)
+
+# Draw four squares to form a larger square
+length = 100
+for i in range(1):
+    draw_square(length)
+    forward(length)
+
+# Wait for user to click before closing the window
+exitonclick()
+
+
+
+
     
 
